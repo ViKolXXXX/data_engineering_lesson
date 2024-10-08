ed8db5b069a37e6bfbd48d8ee2ea2f42d961cd3a
ViKolXXXX <vitek323pvi@rambler.ru>
2024-10-08 17:37:44 +0300

    work lesson 12

--- lesson_12.py
+++ lesson_12.py
@@ -62,14 +62,6 @@ def res():
     print(res_list)
 res()
 
-#  data = {
-# ...    'name': ['Xavier', 'Ann', 'Jana', 'Yi', 'Robin', 'Amal', 'Nori'],
-# ...    'city': ['Mexico City', 'Toronto', 'Prague', 'Shanghai',
-# ...             'Manchester', 'Cairo', 'Osaka'],
-# ...    'age': [41, 28, 33, 34, 38, 31, 37],
-# ...    'py-score': [88.0, 79.0, 81.0, 80.0, 68.0, 61.0, 84.0]
-# ... }
-
 
 
 
