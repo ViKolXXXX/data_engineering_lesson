cafdee056513244deba961ae10421f4d6f341c23
ViKolXXXX <vitek323pvi@rambler.ru>
2024-10-15 15:39:31 +0300

    work

--- lesson_13.py
+++ lesson_13.py
@@ -1,13 +1,19 @@
 # Так это и есть задание после 12 урока - настроить выгрузку информации о курсах валют и о погоде.
 # названия валют и названия городов читать из yaml файлов.
-from datetime import datetime
-import time
 import requests
-import pandas as pd
+
+class Pogoda:
+
+    __access_key = "09cc2d8a-ad09-4f85-9ebd-0001c32e9170"
+
+    def get_pogoda(self):
+        headers = {
+            'X-Yandex-Weather-Key': self.__access_key
+        }
+        moskva = {lat=52.37125&lon=4.89388}
+        response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=52.37125&lon=4.89388', headers=headers)
+        return  response
 
 
-
-URL = 'https://api.gismeteo.net/v2/search/cities/?lang=en&query=москва'
-# response = requests.get(url = URL, headers = {'X-Gismeteo-Token: 56b30cb255.3443075'})
-respo = requests.get(url=URL)
-print(response)
+moskva = Pogoda()
+print(moskva.get_pogoda().json())
